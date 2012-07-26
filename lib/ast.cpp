66a0cb352221e1c369e16b0c7ab7887f00a6189e
Guido Tack <guido.tack@monash.edu>
2012-07-26 14:34:02 +1000

    Fix type checking of polymorphic functions for array arguments

--- lib/ast.cpp
+++ lib/ast.cpp
@@ -480,14 +480,26 @@ namespace MiniZinc {
         }
       }
       if (tii->_ranges && tii->_ranges->size()==1 &&
-          (*tii->_ranges)[0] && (*tii->_ranges)[0]->isa<TIId>()) {
-        CtxStringH tiid = (*tii->_ranges)[0]->cast<TIId>()->_v;
+          (*tii->_ranges)[0]->_domain && 
+          (*tii->_ranges)[0]->_domain->isa<TIId>()) {
+        CtxStringH tiid = (*tii->_ranges)[0]->_domain->cast<TIId>()->_v;
         if (ta[i]->_type._dim<=0) {
           throw TypeError(ta[i]->_loc,"type-inst variable $"+tiid.str()+
             " must be an array index");
         }
         Type tiit = Type::any(ta[i]->_type._dim);
-        tmap.insert(std::pair<CtxStringH,Type>(tiid,tiit));
+        CtxStringMap<Type>::t::iterator it = tmap.find(tiid);
+        if (it==tmap.end()) {
+          tmap.insert(std::pair<CtxStringH,Type>(tiid,tiit));
+        } else {
+          if (it->second._dim == 0) {
+            throw TypeError(ta[i]->_loc,"type-inst variable $"+
+              tiid.str()+" used in both array and non-array position");
+          } else if (it->second!=tiit) {
+            throw TypeError(ta[i]->_loc,"type-inst variable $"+
+              tiid.str()+" instantiated with different types");
+          }
+        }
       }
     }
     if (dh.size() != 0) {
