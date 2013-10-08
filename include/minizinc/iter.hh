9dfe0f180d9b6776bc9ea5659f526c0721215032
Guido Tack <guido.tack@monash.edu>
2013-10-08 16:47:28 +1100

    Add range iterator for set difference

--- include/minizinc/iter.hh
+++ include/minizinc/iter.hh
@@ -255,6 +255,105 @@ namespace MiniZinc { namespace Ranges {
   }
 
   /**
+   * \brief Range iterator for computing set difference
+   *
+   * \ingroup FuncIterRanges
+   */
+
+  template<class I, class J>
+  class Diff : public MinMax {
+  protected:
+    /// Iterator from which to subtract
+    I i;
+    /// Iterator to be subtracted
+    J j;
+  public:
+    /// \name Constructors and initialization
+    //@{
+    /// Default constructor
+    Diff(void);
+    /// Initialize with iterator \a i and \a j
+    Diff(I& i, J& j);
+    /// Initialize with iterator \a i and \a j
+    void init(I& i, J& j);
+    //@}
+
+    /// \name Iteration control
+    //@{
+    /// Move iterator to next range (if possible)
+    void operator ++(void);
+    //@}
+  };
+
+
+
+  template<class I, class J>
+  inline void
+  Diff<I,J>::operator ++(void) {
+    // Precondition: mi <= ma
+    // Task: find next mi greater than ma
+    while (true) {
+      if (!i()) break;
+      mi = ma+1;
+      ma = i.max();
+      if (mi > i.max()) {
+        ++i;
+        if (!i()) break;
+        mi = i.min();
+        ma = i.max();
+      }
+      while (j() && (j.max() < mi))
+        ++j;
+      if (j() && (j.min() <= ma)) {
+        // Now the interval [mi ... ma] must be shrunken
+        // Is [mi ... ma] completely consumed?
+        if ((mi >= j.min()) && (ma <= j.max()))
+          continue;
+        // Does [mi ... ma] overlap on the left?
+        if (j.min() <= mi) {
+          mi = j.max()+1;
+          // Search for max!
+          ++j;
+          if (j() && (j.min() <= ma))
+            ma = j.min()-1;
+        } else {
+          ma = j.min()-1;
+        }
+      }
+      return;
+    }
+    finish();
+  }
+
+  template<class I, class J>
+  inline
+  Diff<I,J>::Diff(void) {}
+
+  template<class I, class J>
+  inline
+  Diff<I,J>::Diff(I& i0, J& j0)
+    : i(i0), j(j0) {
+    if (!i()) {
+      finish();
+    } else {
+      mi = i.min()-1; ma = mi;
+      operator ++();
+    }
+  }
+
+  template<class I, class J>
+  inline void
+  Diff<I,J>::init(I& i0, J& j0) {
+    i = i0; j = j0;
+    if (!i()) {
+      finish();
+    } else {
+      mi = i.min()-1; ma = mi;
+      operator ++();
+    }
+  }
+
+  /**
    * \brief Value iterator from range iterator
    *
    * \ingroup FuncIterValues
