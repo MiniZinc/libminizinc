d94e345e255d389d6b39b6d24844af1fa5b70a38
Guido Tack <guido.tack@monash.edu>
2014-02-22 11:59:50 +1100

    Export paths

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-PATH="$(pwd)/..:":"$(pwd)/scripts":"$PATH"
-MZN_STDLIB_DIR="$(pwd)/../mznlib"
+set -x
+export PATH="$(pwd)/..":"$(pwd)/scripts":"$PATH"
+export MZN_STDLIB_DIR="$(pwd)/../mznlib"
 exec run-tests mzn20_fd .mzn unit examples
