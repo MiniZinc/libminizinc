03b34cc402a6db2b8890fc40630221e35fdd5651
Guido Tack <guido.tack@monash.edu>
2014-05-14 17:42:16 +1000

    Adapt MZN_STDLIB_DIR to new location

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # set -x
 export PATH="$(pwd)/..":"$(pwd)/scripts":"$PATH"
-export MZN_STDLIB_DIR="$(pwd)/../mznlib"
+export MZN_STDLIB_DIR="$(pwd)/../share/minizinc"
 exec run-tests mzn20_fd .mzn unit examples
