899c2cd7a37cbd880bfb546e3f923c66a8acf8d2
Kevin Leo <kevin.kevinleo@gmail.com>
2014-09-26 16:37:46 +1000

    Moved statistics code to lib/statistics.cpp

--- include/minizinc/statistics.hh
+++ include/minizinc/statistics.hh
@@ -14,52 +14,36 @@
 #include <iterator>
 
 namespace MiniZinc {
- 
+
   class Statistics {
-  protected:
-    // time in milliseconds
-    unsigned long long _time;
-    // search nodes
-    unsigned long long _nodes;
-    // failures/ backtracks
-    unsigned long long _failures;
-    // current objective value
-    double _objective;
-    
-  public:
+    protected:
+      // time in milliseconds
+      unsigned long long _time;
+      // search nodes
+      unsigned long long _nodes;
+      // failures/ backtracks
+      unsigned long long _failures;
+      // current objective value
+      double _objective;
+
+    public:
       Statistics() : _time(0), _nodes(0), _failures(0) {}
-    
-      virtual void print(std::ostream& os) {
-        os << "Time(ms):\t"  << _time      << std::endl
-           << "Nodes:\t"     << _nodes     << std::endl
-           << "Failures:\t"  << _failures  << std::endl
-           << "Objective:\t" << _objective << std::endl;
-      };
-      
-      virtual void printLine(std::ostream& os) {
-        os << _time     << '\t'
-           << _nodes    << '\t'
-           << _failures << '\t'
-           << _objective << std::endl;
-      }
-      
-      void time(unsigned long long t) { _time = t; }
-      void nodes(unsigned long long n) { _nodes = n; }
-      void failures(unsigned long long f) { _failures = f; }
-      void objective(double o) { _objective = o; }
-      
-      unsigned long long time() { return _time; };
-      unsigned long long nodes() { return _nodes; };
-      unsigned long long failures() { return _failures; };
-      double objective() { return _objective; };
-            
-      operator+=(Statistics& s) {
-	_time += s.time();
-	_nodes += s.nodes();
-	_failures += s.failures();
-	_objective = s.objective();
-      }
-  };  
+
+      virtual void print(std::ostream& os);
+      virtual void printLine(std::ostream& os);
+
+      void time(unsigned long long t);
+      void nodes(unsigned long long n);
+      void failures(unsigned long long f);
+      void objective(double o);
+
+      unsigned long long time();
+      unsigned long long nodes();
+      unsigned long long failures();
+      double objective();
+
+      operator+=(Statistics& s);
+  };
 }
 
 #endif
