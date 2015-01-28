c43940b5f9e0183e4f325b0d2b27167111dffe06
Andrea Rendl <andrea.rendl@nicta.com.au>
2015-01-28 15:28:59 +1100

    first skeleton of search combinators lite interpreter

---
+++ include/minizinc/search.hh
@@ -0,0 +1,26 @@
+/* -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+ *  Main authors:
+ *     Andrea Rendl <andrea.rendl@nicta.com.au>
+ */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __MINIZINC_SEARCH_HH__
+#define __MINIZINC_SEARCH_HH__
+
+#include "minizinc/flatten.hh"
+#include "minizinc/solver_instance_base.hh"
+
+namespace MiniZinc {
+  
+  /// perform the search on the model environment using the specified solver 
+  template<class SolverInstanceBase>
+  void search(Env& e, MiniZinc::Options& opt);
+  
+}
+
+#endif
---
+++ lib/search.cpp
@@ -0,0 +1,21 @@
+/* -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+
+/*
+ *  Main authors:
+ *     Andrea Rendl <andrea.rendl@nicta.com.au>
+ */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "minizinc/search.hh"
+
+namespace MiniZinc {
+  
+  template<class SolverInstanceBase>
+  void search(Env& env, MiniZinc::Options& opt) {
+    // TODO: implement    
+  }
+  
+}
