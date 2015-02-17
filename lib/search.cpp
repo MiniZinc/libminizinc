e14ced2f73d1bba03e8a2a6474c2da8bb479e1d8
Andrea Rendl <andrea.rendl@nicta.com.au>
2015-02-17 16:13:08 +1100

    fixed AND and OR combinator interpretation (using arrays)

--- lib/search.cpp
+++ lib/search.cpp
@@ -21,30 +21,51 @@ namespace MiniZinc {
     
     if(Call* call = comb->dyn_cast<Call>()) {      
       if(call->id().str() == constants().combinators.and_.str()) {
-        std::cout << "DEBUG: AND combinator: " << (*call) << std::endl;
-        // TODO: AND combinator takes an array as argument
-        for(unsigned int i=0; i<call->args().size(); i++) {
-          SolverInstance::Status status = interpretCombinator(call->args()[i],env,solver);
-          if(status != SolverInstance::SAT)
-            return status;
+        std::cout << "DEBUG: AND combinator: " << (*call) << std::endl;        
+        if(call->args().size() != 1) {
+          std::stringstream ssm;
+          ssm << "AND-combinator only takes 1 argument instead of " << call->args().size() << " in: " << *call;
+          throw TypeError(call->loc(), ssm.str());
         }
+        if(ArrayLit* al = call->args()[0]->dyn_cast<ArrayLit>()) {
+          assert(al->dims() == 1);
+          for(unsigned int i=0; i<al->length(); i++) {
+            SolverInstance::Status status = interpretCombinator(al->v()[i],env,solver);
+            if(status != SolverInstance::SAT)
+              return status;
+          }
+        } else {
+          std::stringstream ssm;
+          ssm << "AND-combinator takes an array as argument";
+          throw TypeError(call->loc(), ssm.str());
+        }        
       } 
       else if(call->id().str() == constants().combinators.or_.str()) {
-        std::cout << "DEBUG: OR combinator: " << *call  << std::endl;
-        bool oneIsFeasible = false;
-        SolverInstance::Status status;
-        // TODO: OR combinator takes an array as argument
-        for(unsigned int i=0; i<call->args().size(); i++) {
-          status = interpretCombinator(call->args()[i],env,solver);
-          if(status == SolverInstance::SAT)
-            oneIsFeasible = true;
+        std::cout << "DEBUG: OR combinator: " << (*call) << std::endl;        
+        if(call->args().size() != 1) {
+          std::stringstream ssm;
+          ssm << "OR-combinator only takes 1 argument instead of " << call->args().size() << " in: " << *call;
+          throw TypeError(call->loc(), ssm.str());
         }
-        // TODO: not sure about the behaviour of OR combinator...
-        return oneIsFeasible ? SolverInstance::SAT : status;
+        SolverInstance::Status status = SolverInstance::UNKNOWN;
+        bool oneIsFeasible = false;
+        if(ArrayLit* al = call->args()[0]->dyn_cast<ArrayLit>()) {
+          assert(al->dims() == 1);
+          for(unsigned int i=0; i<al->length(); i++) {
+            status = interpretCombinator(al->v()[i],env,solver);
+            if(status == SolverInstance::SAT)
+              oneIsFeasible = true;
+          }
+          return oneIsFeasible ? SolverInstance::SAT : status;
+        } else {
+          std::stringstream ssm;
+          ssm << "OR-combinator takes an array as argument";
+          throw TypeError(call->loc(), ssm.str());
+        }                
       }
       else if(call->id().str() == constants().combinators.post.str()) {
         std::cout << "DEBUG: POST combinator: " << *call << std::endl;
-        // TODO: post constraint in solver
+        // TODO: post constraint in solver        
         return SolverInstance::UNKNOWN;
       }
       else if(call->id().str() == constants().combinators.repeat.str()) {
@@ -57,13 +78,13 @@ namespace MiniZinc {
           return status;
         }
         else if(call->args().size() == 2) { // repeat restriction: TODO: check the format-> maybe it is a comprehension
+          // TODO: implement
         }
         else {
           std::stringstream ssm;
           ssm << "REPEAT-combinator only takes 1 or 2 arguments instead of " << call->args().size() << " in: " << *call;
           throw TypeError(call->loc(), ssm.str());
-        }
-        // TODO: repeat the arguments
+        }        
       }
       else if(call->id().str() == constants().combinators.scope.str()) {
         std::cout << "DEBUG: SCOPE combinator: " << *call << std::endl;       
