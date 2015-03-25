430ffdf45c424fa151cbb131d7c84beccd4db068
Guido Tack <guido.tack@monash.edu>
2015-03-25 16:55:57 +1100

    Need to follow Ids to their declaration before flattening them

--- lib/flatten.cpp
+++ lib/flatten.cpp
@@ -2497,6 +2497,7 @@ namespace MiniZinc {
     case Expression::E_ID:
       {
         Id* id = e->cast<Id>();
+        id = follow_id_to_decl(id)->cast<VarDecl>()->id();
         if (id->decl()==NULL) {
           if (id->type().isann()) {
             ret.b = bind(env,Ctx(),b,constants().lit_true);
