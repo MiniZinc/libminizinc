9c6123648b5cdf27759982f4d521e2d77c61f1bb
Guido Tack <guido.tack@monash.edu>
2015-04-30 16:14:42 +1000

    Better bounds for if-then-else

--- lib/eval_par.cpp
+++ lib/eval_par.cpp
@@ -1374,9 +1374,36 @@ namespace MiniZinc {
           valid = false;
         }
         return false;
-      } else {
-        return e->type().isint();
       }
+      if (ITE* ite = e->dyn_cast<ITE>()) {
+        Bounds itebounds(IntVal::infinity, -IntVal::infinity);
+        for (unsigned int i=0; i<ite->size(); i++) {
+          if (ite->e_if(i)->type().ispar() && ite->e_if(i)->type().cv()==Type::CV_NO) {
+            if (eval_bool(env, ite->e_if(i))) {
+              BottomUpIterator<ComputeIntBounds> cbi(*this);
+              cbi.run(ite->e_then(i));
+              Bounds& back = _bounds.back();
+              back.first = std::min(itebounds.first, back.first);
+              back.second = std::max(itebounds.second, back.second);
+              return false;
+            }
+          } else {
+            BottomUpIterator<ComputeIntBounds> cbi(*this);
+            cbi.run(ite->e_then(i));
+            Bounds back = _bounds.back();
+            _bounds.pop_back();
+            itebounds.first = std::min(itebounds.first, back.first);
+            itebounds.second = std::max(itebounds.second, back.second);
+          }
+        }
+        BottomUpIterator<ComputeIntBounds> cbi(*this);
+        cbi.run(ite->e_else());
+        Bounds& back = _bounds.back();
+        back.first = std::min(itebounds.first, back.first);
+        back.second = std::max(itebounds.second, back.second);
+        return false;
+      }
+      return e->type().isint();
     }
     /// Visit integer literal
     void vIntLit(const IntLit& i) {
