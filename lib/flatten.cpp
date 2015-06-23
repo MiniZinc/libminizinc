b01a4e270c3466247c6440a57ad7b7c976a321d6
Guido Tack <guido.tack@monash.edu>
2015-06-23 16:54:22 +1000

    Recursively copy functions used in output

--- lib/flatten.cpp
+++ lib/flatten.cpp
@@ -5039,6 +5039,7 @@ namespace MiniZinc {
                 topDown(ce, decl->params()[i]);
               env.output->registerFn(env, decl);
               env.output->addItem(decl);
+              topDown(*this, decl->e());
             } else {
               decl = origdecl;
             }
