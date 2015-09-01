e5f2ef6d9b135892d39fbc49e3b0eef6a5f5f6db
Guido Tack <guido.tack@monash.edu>
2015-09-01 14:50:07 +0100

    Change pretty printer to use one less digit when printing float literals. This fixes #41 (or at least provides a workaround), but some double constants may still be rounded incorrectly when pretty printing and reading them back in. The real fix will be to output hex float literals (coming soon).

--- lib/prettyprinter.cpp
+++ lib/prettyprinter.cpp
@@ -207,7 +207,7 @@ namespace MiniZinc {
       case Expression::E_FLOATLIT:
         {
           std::ostringstream oss;
-          oss << std::setprecision(std::numeric_limits<double>::digits10+2);
+          oss << std::setprecision(std::numeric_limits<double>::digits10+1);
           oss << e->cast<FloatLit>()->v();
           if (oss.str().find("e") == std::string::npos && oss.str().find(".") == std::string::npos)
             oss << ".0";
@@ -1045,7 +1045,7 @@ namespace MiniZinc {
     }
     ret mapFloatLit(const FloatLit& fl) {
       std::ostringstream oss;
-      oss << std::setprecision(std::numeric_limits<double>::digits10+2);
+      oss << std::setprecision(std::numeric_limits<double>::digits10+1);
       oss << fl.v();
       if (oss.str().find("e") == std::string::npos && oss.str().find(".") == std::string::npos)
         oss << ".0";
