729b3e70b510437177d8dd99482f3b296bfeb41d
Guido Tack <guido.tack@monash.edu>
2015-10-15 10:18:04 +1100

    Remove vardecl right hand sides when they are fixed during optimisation

--- lib/optimize.cpp
+++ lib/optimize.cpp
@@ -590,7 +590,7 @@ namespace MiniZinc {
       if (bi->isa<ConstraintI>()) {
         c = bi->cast<ConstraintI>()->e()->dyn_cast<Call>();
       } else {
-        c = bi->cast<VarDeclI>()->e()->e()->dyn_cast<Call>();
+        c = Expression::dyn_cast<Call>(bi->cast<VarDeclI>()->e()->e());
       }
       if (c==NULL)
         continue;
@@ -1257,6 +1257,7 @@ namespace MiniZinc {
                 env.flat()->fail(env);
                 vdi->e()->e(constants().boollit(isConjunction));
               }
+              toRemove.push_back(vdi);
             }
           } else {
             // not subsumed, nonfixed==1
