6e8e73282ddcfa7c4f3ac3c6210ea380dbdd3223
unknown <glebb@MU00061993.AD.MONASH.EDU>
2016-01-05 17:42:08 +1100

    For Release config in MSVC: cmake --build . --config Release

--- BUILD.bat
+++ BUILD.bat
@@ -1,4 +1,4 @@
 REM This Windows batch is to run from the build subdirectory
-REM Release is not used, need to switch on manually in MSVC
 cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCPLEX_STUDIO_DIR="C:\Program Files\IBM\ILOG\CPLEX_Studio1262"  
-cmake --build .
+REM Release cannot be specified from config for MSVC, so doing it for build (can also switch on in IDE)
+cmake --build . --config Release
