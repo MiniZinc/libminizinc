c61079895eb5cfb57a567d51b4bef45e34d7db7b
gleb_win <gleb.belov@monash.edu>
2016-01-06 14:10:35 +1100

    mzn-cbc on Win 64. CBC has MSVC 2010 project files which is fine.

--- BUILD.bat
+++ BUILD.bat
@@ -1,4 +1,4 @@
 REM This Windows batch is to run from the build subdirectory
-cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCPLEX_STUDIO_DIR="C:\Program Files\IBM\ILOG\CPLEX_Studio1262"  
+cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCPLEX_STUDIO_DIR="C:\Program Files\IBM\ILOG\CPLEX_Studio1262" -DOSICBC_HOME="C:\Users\glebb\prj\coin-Cbc" 
 REM Release cannot be specified from config for MSVC, so doing it for build (can also switch on in IDE)
 cmake --build . --config Release
--- BUILD.sh
+++ BUILD.sh
@@ -2,7 +2,7 @@
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON \
            -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1262 \
            -DGUROBI_HOME=/opt/gurobi605/linux64 \
-           -DOSICBC_HOME=$OSICBC_HOME '# -DOSICBC_LINKEXTRAS  necessary on ArchLinux' \
+           -DOSICBC_HOME=$OSICBC_HOME -DOSICBC_LINKEXTRAS="" '# bz2 lapack blas  necessary on ArchLinux' \
            -DSCIP_DIR=$SCIP_DIR -DSOPLEX_DIR=$SOPLEX_DIR -DZIMPL_DIR=$ZIMPL_DIR \
            ..
 #cmake -DCMAKE_BUILD_TYPE=debug  -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1261 ..
