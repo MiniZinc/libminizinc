ea5e1c5814c4106d3384883f86f48c83e0af385b
gleb_win <gleb.belov@monash.edu>
2016-01-06 15:08:36 +1100

    Gurobi 6.5 on Win

--- BUILD.bat
+++ BUILD.bat
@@ -1,4 +1,4 @@
 REM This Windows batch is to run from the build subdirectory
-cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCPLEX_STUDIO_DIR="C:\Program Files\IBM\ILOG\CPLEX_Studio1262" -DOSICBC_HOME="C:\Users\glebb\prj\coin-Cbc" 
+cmake .. -G"Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCPLEX_STUDIO_DIR="C:\Program Files\IBM\ILOG\CPLEX_Studio1262" -DOSICBC_HOME="C:\Users\glebb\prj\coin-Cbc" -DGUROBI_HOME=%GUROBI_HOME%
 REM Release cannot be specified from config for MSVC, so doing it for build (can also switch on in IDE)
 cmake --build . --config Release
--- BUILD.sh
+++ BUILD.sh
@@ -1,7 +1,7 @@
 # This shell script is to run from the build subdirectory
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON \
            -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1262 \
-           -DGUROBI_HOME=/opt/gurobi605/linux64 \
+           -DGUROBI_HOME=$GUROBI_HOME \
            -DOSICBC_HOME=$OSICBC_HOME -DOSICBC_LINKEXTRAS="" '# bz2 lapack blas  necessary on ArchLinux' \
            -DSCIP_DIR=$SCIP_DIR -DSOPLEX_DIR=$SOPLEX_DIR -DZIMPL_DIR=$ZIMPL_DIR \
            ..
