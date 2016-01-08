bef7ed4257818916a85ab8e9fc1b1049027b8d41
Gleb Belov <gleb.belov@monash.edu>
2016-01-08 17:22:26 +1100

    Changed the test script to run mzn-cplex only   TODO

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -2,4 +2,4 @@
 # set -x
 export PATH="$(pwd)/..":"$(pwd)/scripts":"$PATH"
 export MZN_STDLIB_DIR="$(pwd)/../share/minizinc"
-exec run-tests mzn20_fd .mzn unit examples
+exec run-tests mzn20_mip .mzn unit examples
