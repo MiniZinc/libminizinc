ebd219ad3c94055b757a0505d59978efce3c1763
Gleb Belov <gleb.belov@monash.edu>
2016-01-27 16:37:11 +1100

    Switch to CPLEX 12.6.3

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,6 @@
 # This shell script is to run from the build subdirectory
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON \
-           -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1262 \
+           -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1263 \
            -DGUROBI_HOME=$GUROBI_HOME \
            -DOSICBC_HOME=$OSICBC_HOME -DOSICBC_LINKEXTRAS="" '# bz2 lapack blas  necessary on ArchLinux' \
            -DSCIP_DIR=$SCIP_DIR -DSOPLEX_DIR=$SOPLEX_DIR -DZIMPL_DIR=$ZIMPL_DIR \
