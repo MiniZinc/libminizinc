077c161f00397eeeb6d188d27bef44c87d327d8b
Jip J. Dekker <jip@dekker.li>
2016-02-05 05:12:26 +0100

    Adds the finding of calls to presolved predicates

--- include/minizinc/presolve.hh
+++ include/minizinc/presolve.hh
@@ -16,7 +16,85 @@
 
 namespace MiniZinc {
 
+  class SubModel {
+  public:
+    enum Strategy {INSTANCE, MODEL, GLOBAL};
+  protected:
+    // predicate around which the submodel revolves
+    FunctionI * _predicate;
+// calls to the predicate in the model
+    std::vector<Call*> _calls;
+    // submodel presolve strategy
+    Strategy _strategy;
+    // save/load result from file?
+    bool _save;
+
+  public:
+    SubModel(FunctionI* pred, Strategy strategy, bool save) : _predicate(pred), _save(save), _strategy(strategy) {};
+
+    FunctionI *p() const { return _predicate; }
+
+    void p(FunctionI *p) { SubModel::_predicate = p; }
+
+
+    const std::vector<Call *> &c() const { return _calls; }
+
+    void addCall(Call* c) { _calls.push_back(c); }
+  };
+
   void presolve(Env& env, Model* m);
 
+  void find_presolve_annotations(Env& env, Model* m, std::vector<SubModel>& submodels);
+
+  void find_presolved_calls(Env& env, Model* m, std::vector<SubModel>& submodels);
+
+//  TODO: Make Expression visitors more like ItemVisitors, so we don't need all the function definitions.
+  class CallFinder {
+  public:
+    EnvI& env;
+    Model* m;
+    std::vector<SubModel>& submodels;
+    CallFinder(EnvI& env0, Model* m0, std::vector<SubModel>& submodels0) : env(env0), m(m0), submodels(submodels0) {};
+    /// Check annotations when expression is finished
+    void exit(Expression* e) {}
+    bool enter(Expression*) { return true; }
+    /// Visit integer literal
+    void vIntLit(const IntLit&) {}
+    /// Visit floating point literal
+    void vFloatLit(const FloatLit&) {}
+    /// Visit Boolean literal
+    void vBoolLit(const BoolLit&) {}
+    /// Visit set literal
+    void vSetLit(SetLit& sl) {}
+    /// Visit string literal
+    void vStringLit(const StringLit&) {}
+    /// Visit identifier
+    void vId(Id& id) {}
+    /// Visit anonymous variable
+    void vAnonVar(const AnonVar&) {}
+    /// Visit array literal
+    void vArrayLit(ArrayLit& al) {}
+    /// Visit array access
+    void vArrayAccess(ArrayAccess& aa) {}
+    /// Visit array comprehension
+    void vComprehension(Comprehension& c) {}
+    /// Visit array comprehension generator
+    void vComprehensionGenerator(Comprehension& c, int gen_i) {}
+    /// Visit if-then-else
+    void vITE(ITE& ite) {}
+    /// Visit binary operator
+    void vBinOp(BinOp& bop) {}
+    /// Visit unary operator
+    void vUnOp(UnOp& uop) {}
+    /// Visit call
+    void vCall(Call& call);
+    /// Visit let
+    void vLet(Let& let) {}
+    /// Visit variable declaration
+    void vVarDecl(VarDecl& vd) {}
+    /// Visit type inst
+    void vTypeInst(TypeInst& ti) {}
+    void vTIId(TIId& id) {}
+  };
 }
 #endif
--- lib/presolve.cpp
+++ lib/presolve.cpp
@@ -10,18 +10,71 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <minizinc/presolve.hh>
+#include <minizinc/astexception.hh>
+#include <minizinc/astiterator.hh>
 
 void MiniZinc::presolve(Env& env, Model* m) {
+  std::vector<SubModel> submodels;
+
+  find_presolve_annotations(env, m, submodels);
+  if (submodels.size() < 1) return;
+
+  find_presolved_calls(env, m, submodels);
+
+  m->compact();
+}
+
+void MiniZinc::find_presolve_annotations(Env &env, Model *m, std::vector<SubModel> &submodels) {
   class GoalSeeker : public ItemVisitor {
   public:
+    EnvI& env;
+    std::vector<SubModel>& submodels;
+    GoalSeeker(EnvI& env0, std::vector<SubModel>& submodels0) : env(env0), submodels(submodels0) {};
     void vFunctionI(FunctionI* i) {
       Expression* ann = getAnnotation(i->ann(),constants().presolve.presolve);
       if (ann) {
         ASTExprVec<Expression> args = ann->cast<Call>()->args();
-        Id* strategy = args[0]->cast<Id>();
+        Id* s_id = args[0]->cast<Id>();
         bool save = args[1]->cast<BoolLit>()->v();
+
+        SubModel::Strategy strategy;
+        if (s_id->v() == constants().presolve.instance->v())
+          strategy = SubModel::INSTANCE;
+        else if (s_id->v() == constants().presolve.model->v())
+          strategy = SubModel::MODEL;
+        else if (s_id->v() == constants().presolve.global->v())
+          strategy = SubModel::GLOBAL;
+        else
+          throw TypeError(this->env, s_id->loc(), "Invalid presolve strategy `" + s_id->str().str() + "'");
+
+        submodels.push_back(SubModel(i, strategy, save));
+        i->remove();
       }
     }
-  } goals;
+  } goals(env.envi(),submodels);
   iterItems(goals,m);
+}
+
+void MiniZinc::find_presolved_calls(Env& env, Model* m, std::vector<SubModel>& submodels) {
+  CallFinder cf = CallFinder(env.envi(), m, submodels);
+  TopDownIterator<CallFinder> cf_it(cf);
+
+  class CallSeeker : public ItemVisitor {
+  public:
+    EnvI& env;
+    TopDownIterator<CallFinder>& cf;
+    CallSeeker(EnvI& env0, TopDownIterator<CallFinder>& cf0) : env(env0), cf(cf0) {};
+    void vConstraintI(ConstraintI* i) {
+      cf.run(i->e());
+    }
+  } cs(env.envi(), cf_it);
+  iterItems(cs, m);
+}
+
+void MiniZinc::CallFinder::vCall(Call &call) {
+  for (size_t i = 0; i < submodels.size(); ++i) {
+    if (submodels[i].p() == m->matchFn(env, &call)) {
+      submodels[i].addCall(&call);
+    }
+  }
 }
