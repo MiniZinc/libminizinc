4c516714c1406e864d3dac764bc797fd6ff728e5
Gleb Belov <gleb.belov@monash.edu>
2016-02-19 16:17:38 +1100

    INSTALL.txt, secion "Running": info about available mzn-* drivers and options -v, -s, -a because users seem to not know this

--- BUILD.sh
+++ BUILD.sh
@@ -1,10 +1,10 @@
 # This shell script is to run from the build subdirectory
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_VERBOSE_MAKEFILE=ON \
-           -DCCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1263 \
-           -DCGUROBI_HOME=$GUROBI_HOME \
-           -DCOSICBC_HOME=$OSICBC_HOME -DOSICBC_LINKEXTRAS="" '# bz2 lapack blas  necessary on ArchLinux' \
-           -DCSCIP_DIR=$SCIP_DIR -DSOPLEX_DIR=$SOPLEX_DIR -DZIMPL_DIR=$ZIMPL_DIR \
-           -DCGECODE_HOME=/home/bg/Documents/prj/gecode-4.4.0 \
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON \
+           -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1263 \
+           -DGUROBI_HOME=$GUROBI_HOME \
+           -DOSICBC_HOME=$OSICBC_HOME -DOSICBC_LINKEXTRAS="" '# bz2 lapack blas  necessary on ArchLinux' \
+           -DSCIP_DIR=$SCIP_DIR -DSOPLEX_DIR=$SOPLEX_DIR -DZIMPL_DIR=$ZIMPL_DIR \
+           -DGECODE_HOME=/home/bg/Documents/prj/gecode-4.4.0 \
            ..
 #cmake -DCMAKE_BUILD_TYPE=debug  -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1261 ..
 cmake --build . -- -j3
