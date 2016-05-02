a373023dd364fd73b7bc95dc0e05f888af15c870
Guido Tack <guido.tack@monash.edu>
2016-05-02 18:14:56 +1000

    Only create new partiality variable for if-then-else expression if there's at least one var condition

--- lib/flatten.cpp
+++ lib/flatten.cpp
@@ -1771,11 +1771,6 @@ namespace MiniZinc {
     
     VarDecl* nr = r;
 
-    if (b==NULL) {
-      b = newVarDecl(env, Ctx(), new TypeInst(Location().introduce(),Type::varbool()), NULL, NULL, NULL);
-    }
-    
-
     Ctx cmix;
     cmix.b = C_MIX;
     cmix.i = C_MIX;
@@ -1855,7 +1850,11 @@ namespace MiniZinc {
           eq_then = new BinOp(Location().introduce(),nr->id(),BOT_EQ,ethen.r());
           eq_then->type(Type::varbool());
         }
-        
+
+        if (b==NULL) {
+          b = newVarDecl(env, Ctx(), new TypeInst(Location().introduce(),Type::varbool()), NULL, NULL, NULL);
+        }
+
         {
           // Create a clause with all the previous conditions negated, the
           // current condition, and the then branch.
