5cd376c3bff6608c0716ed8e0250c5fdb2014358
Jip J. Dekker <jip@dekker.li>
2016-09-07 16:43:33 +0200

    Cleans up the computation of the domain expressions, supporting array access directly

--- lib/presolve_utils.cpp
+++ lib/presolve_utils.cpp
@@ -82,68 +82,77 @@ namespace MiniZinc{
   }
 
   Expression* computeDomainExpr(EnvI& env, Expression* exp) {
-//    TODO: Should we compute the unions manually?
-    if (exp->eid() == Expression::E_ID )
-      return exp->cast<Id>()->decl()->ti()->domain();
     Expression* type = nullptr;
-    if(exp->type().dim() > 0) {
-//      TODO: Use eval_array_lit instead?
-      switch (exp->eid()) {
-        case Expression::E_ARRAYLIT: {
-          ArrayLit* arr = exp->cast<ArrayLit>();
-          if (arr->length() > 0) {
-            type = computeDomainExpr(env, arr->v()[0]);
-            if (!type) return nullptr;
-            for (int i = 1; i < arr->length(); ++i) {
-              Expression* type2 = computeDomainExpr(env, arr->v()[i]);
-              if(!type2) return nullptr;
-              type = new BinOp(Location(), type, BOT_UNION, type2);
-              type->type( type2->type() );
-            }
-          }
-          return type;
-        }
-        case Expression::E_CALL: {
-          Call* c = exp->cast<Call>();
-          if ( c->id().str().find("array")  ) {
-            return c->args()[c->args().size() == 1 ? 0 : 1]->cast<ArrayLit>();
-          } else {
-//            TODO: Are there more cases in which we can compute this?
-            return nullptr;
-          }
-        }
-        case Expression::E_BINOP: {
-          BinOp* op = exp->cast<BinOp>();
-          if (op->op() == BinOpType::BOT_PLUSPLUS) {
-            Expression* left = computeDomainExpr(env, op->lhs());
-            Expression* right = computeDomainExpr(env, op->rhs());
-            if (!left || !right) return nullptr;
-            type = new BinOp(Location(), left, BOT_UNION, right);
-            type->type( left->type() );
-          } else {
-            return nullptr;
-          }
-        }
-        case Expression::E_COMP: {
-          Comprehension* comp = exp->cast<Comprehension>();
-          return computeDomainExpr(env, comp->e());
-        }
-        default: {
-//          TODO: Print expression for error
-          throw InternalError("Presolver couldn't determine counds of expression");
+    switch (exp->eid()) {
+      case Expression::E_ID: {
+        VarDecl* vd = follow_id_to_decl(exp)->dyn_cast<VarDecl>();
+        if (vd) {
+          return vd->ti()->domain();
+        } else {
+          break;
         }
       }
+      case Expression::E_ARRAYACCESS: {
+        ArrayAccess* aa = exp->cast<ArrayAccess>();
+        if (Id* id = aa->v()->dyn_cast<Id>()) {
+          if (id->decl()->ti()->domain()) {
+            return id->decl()->ti()->domain();
+          }
+        }
+        break;
+      }
+      case Expression::E_ARRAYLIT: {
+        ArrayLit* arr = exp->cast<ArrayLit>();
+        if (arr->length() > 0) {
+          type = computeDomainExpr(env, arr->v()[0]);
+          if (!type) return nullptr;
+          for (int i = 1; i < arr->length(); ++i) {
+            Expression* type2 = computeDomainExpr(env, arr->v()[i]);
+            if (!type2) return nullptr;
+            type = new BinOp(Location(), type, BOT_UNION, type2);
+            type->type(type2->type());
+          }
+        }
+        return type;
+      }
+      case Expression::E_CALL: {
+        Call* c = exp->cast<Call>();
+        if (c->id().str().find("array")) {
+          return c->args()[c->args().size() == 1 ? 0 : 1]->cast<ArrayLit>();
+        } else {
+//            TODO: Are there more cases in which we can give more exact domains?
+          break;
+        }
+      }
+      case Expression::E_BINOP: {
+        BinOp* op = exp->cast<BinOp>();
+        if (op->op() == BinOpType::BOT_PLUSPLUS) {
+          Expression* left = computeDomainExpr(env, op->lhs());
+          Expression* right = computeDomainExpr(env, op->rhs());
+          if (!left || !right) return nullptr;
+          type = new BinOp(Location(), left, BOT_UNION, right);
+          type->type(left->type());
+        } else {
+          break;
+        }
+      }
+      case Expression::E_COMP: {
+        Comprehension* comp = exp->cast<Comprehension>();
+        return computeDomainExpr(env, comp->e());
+      }
+      default: {
+        break;
+      }
     }
-//    TODO: How about booleans?
     switch (exp->type().bt()) {
       case Type::BT_INT: {
         IntBounds ib = compute_int_bounds(env, exp);
         if (ib.valid) {
-          type = new SetLit( Location(), IntSetVal::a(ib.l, ib.u) );
+          type = new SetLit(Location(), IntSetVal::a(ib.l, ib.u));
           type->type(Type::parsetint());
           return type;
         }
-        break;
+        return nullptr;
       }
       case Type::BT_FLOAT: {
         FloatBounds ib = compute_float_bounds(env, exp);
@@ -152,12 +161,11 @@ namespace MiniZinc{
           type->type(Type::parsetfloat());
           return type;
         }
-        break;
+        return nullptr;
       }
       default:
-        return nullptr;
+        throw InternalError("Presolver couldn't determine domain of expression");
     }
-    return nullptr;
   }
 
   void computeRanges(EnvI& env, Expression* exp, std::vector<TypeInst*>& ranges) {
