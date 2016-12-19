3d8fd644462526fa68599ea45b93a51355356fe2
Guido Tack <guido.tack@monash.edu>
2016-12-19 13:40:24 +0100

    HTML layout improvements

--- en/conf.py
+++ en/conf.py
@@ -115,7 +115,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'manni'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
@@ -129,16 +129,24 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
+import sphinx_bootstrap_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    
+    'source_link_position': "",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -212,7 +220,7 @@ html_static_path = ['_static']
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #
-# html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #
