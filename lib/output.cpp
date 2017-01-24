85a8521feb7ff8712c700e7eec9630e2b7591992
Guido Tack <guido.tack@monash.edu>
2017-01-24 12:17:15 +1100

    Need to turn renamed output variables into par. Fixes problems with output of variables that have quoted identifiers.

--- lib/output.cpp
+++ lib/output.cpp
@@ -213,6 +213,7 @@ namespace MiniZinc {
       TypeInst* vd_rename_ti = copy(e,e.cmap,vd->ti())->cast<TypeInst>();
       VarDecl* vd_rename = new VarDecl(Location().introduce(), vd_rename_ti, vd->flat()->id()->idn(), NULL);
       vd_rename->flat(vd->flat());
+      makePar(e,vd_rename);
       vd->e(vd_rename->id());
       e.output->addItem(new VarDeclI(Location().introduce(), vd_rename));
     }
