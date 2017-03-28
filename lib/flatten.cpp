513a59144fd22c57d5674df77ebdb7fb8d608852
Guido Tack <guido.tack@monash.edu>
2017-03-28 13:39:20 +1100

    Mark domain as "not computed" to avoid constraints being removed that are not entailed. Fixes #150.

--- lib/flatten.cpp
+++ lib/flatten.cpp
@@ -1204,7 +1204,12 @@ namespace MiniZinc {
                         if (newdom->size()==0) {
                           env.fail();
                         } else {
-                          vdi->ti()->domain(new SetLit(Location().introduce(),newdom));
+                          IntSetRanges vdi_domr2(vdi_dom);
+                          IntSetRanges newdomr(newdom);
+                          if (!Ranges::equal(vdi_domr2, newdomr)) {
+                            vdi->ti()->domain(new SetLit(Location().introduce(),newdom));
+                            vdi->ti()->setComputedDomain(false);
+                          }
                         }
                       }
                     }
@@ -1225,7 +1230,12 @@ namespace MiniZinc {
                         if (newdom->size()==0) {
                           env.fail();
                         } else {
-                          vdi->ti()->domain(new SetLit(Location().introduce(),newdom));
+                          FloatSetRanges vdi_domr2(vdi_dom);
+                          FloatSetRanges newdomr(newdom);
+                          if (!Ranges::equal(vdi_domr2, newdomr)) {
+                            vdi->ti()->domain(new SetLit(Location().introduce(),newdom));
+                            vdi->ti()->setComputedDomain(false);
+                          }
                         }
                       }
                     }
