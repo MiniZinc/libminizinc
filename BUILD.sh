13b6b4e98b298324c42af4380e77c3cbfbf2e937
Gleb Belov <gleb.belov@monash.edu>
2017-04-26 12:08:36 +1000

    BUILD.sh uses GECODE_HOME environment variable

--- BUILD.sh
+++ BUILD.sh
@@ -11,7 +11,7 @@ cmake -DCMAKE_BUILD_TYPE=$BLD_TYPE -DCMAKE_VERBOSE_MAKEFILE=ON \
            -DGUROBI_HOME=$GUROBI_HOME -DBUILD_GUROBI_PLUGIN=ON \
            -DOSICBC_HOME=$OSICBC_HOME -DOSICBC_LINKEXTRAS="" '# bz2 lapack blas  necessary on ArchLinux' \
            -DSCIP_DIR=$SCIP_DIR -DSOPLEX_DIR=$SOPLEX_DIR -DZIMPL_DIR=$ZIMPL_DIR \
-           -DGECODE_HOME=/home/bg/Documents/prj/gecode-4.4.0 \
+           -DGECODE_HOME=$GECODE_HOME \
            ..
 #cmake -DCMAKE_BUILD_TYPE=debug  -DCPLEX_STUDIO_DIR=/opt/ibm/ILOG/CPLEX_Studio1261 ..
 cmake --build . -- -j3
