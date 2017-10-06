9aa1173caad0a6fc6c28b0401842d776c6dd16ea
Guido Tack <guido.tack@monash.edu>
2017-10-06 17:04:11 +1100

    Fix incorrect simplification of float domain constraints. Fixes #159.

--- include/minizinc/flatten_internal.hh
+++ include/minizinc/flatten_internal.hh
@@ -341,8 +341,7 @@ namespace MiniZinc {
       FloatSetVal* ndomain;
       switch (bot) {
         case BOT_LE:
-          v -= 1;
-          // fall through
+          return NULL;
         case BOT_LQ:
         {
           Ranges::Bounded<FloatVal,FloatSetRanges> b = Ranges::Bounded<FloatVal,FloatSetRanges>::maxiter(dr,v);
@@ -350,8 +349,7 @@ namespace MiniZinc {
         }
           break;
         case BOT_GR:
-          v += 1;
-          // fall through
+          return NULL;
         case BOT_GQ:
         {
           Ranges::Bounded<FloatVal,FloatSetRanges> b = Ranges::Bounded<FloatVal,FloatSetRanges>::miniter(dr,v);
