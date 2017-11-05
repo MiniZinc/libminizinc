e3d16f72ecdd65793eb297330da30f910b0a8d62
Guido Tack <guido.tack@monash.edu>
2017-11-05 17:22:01 +1100

    Split up var where clauses that are conjunctions of var and par expressions, and evaluate par expressions during iteration (rather than compiling them into the optionality constraints)

--- lib/flatten.cpp
+++ lib/flatten.cpp
@@ -3611,8 +3611,53 @@ namespace MiniZinc {
               in[i] = c->in(i);
             }
             if (c->where(i) && c->where(i)->type().isvar()) {
-              orig_where[i] = NULL;
-              where.push_back(c->where(i));
+              // This is a generalised where clause. Split into par and var part.
+              // The par parts can remain in where clause. The var parts are translated
+              // into optionality constraints.
+              if (c->where(i)->isa<BinOp>() && c->where(i)->cast<BinOp>()->op()==BOT_AND) {
+                std::vector<Expression*> parWhere;
+                std::vector<BinOp*> todo;
+                todo.push_back(c->where(i)->cast<BinOp>());
+                while (!todo.empty()) {
+                  BinOp* bo = todo.back();
+                  todo.pop_back();
+                  if (bo->rhs()->type().ispar()) {
+                    parWhere.push_back(bo->rhs());
+                  } else if (bo->rhs()->isa<BinOp>() && bo->rhs()->cast<BinOp>()->op()==BOT_AND) {
+                    todo.push_back(bo->rhs()->cast<BinOp>());
+                  } else {
+                    where.push_back(bo->rhs());
+                  }
+                  if (bo->lhs()->type().ispar()) {
+                    parWhere.push_back(bo->lhs());
+                  } else if (bo->lhs()->isa<BinOp>() && bo->lhs()->cast<BinOp>()->op()==BOT_AND) {
+                    todo.push_back(bo->lhs()->cast<BinOp>());
+                  } else {
+                    where.push_back(bo->lhs());
+                  }
+                }
+                switch (parWhere.size()) {
+                  case 0:
+                    orig_where[i] = NULL;
+                    break;
+                  case 1:
+                    orig_where[i] = parWhere[0];
+                    break;
+                  case 2:
+                    orig_where[i] = new BinOp(c->where(i)->loc(), parWhere[0], BOT_AND, parWhere[1]);
+                    orig_where[i]->type(Type::parbool());
+                  default:
+                  {
+                    Call* forall = new Call(c->where(i)->loc(), constants().ids.forall, parWhere);
+                    forall->type(Type::parbool());
+                    forall->decl(env.orig->matchFn(env, forall, false));
+                    orig_where[i] = forall;
+                  }
+                }
+              } else {
+                orig_where[i] = NULL;
+                where.push_back(c->where(i));
+              }
             } else {
               orig_where[i] = c->where(i);
             }
