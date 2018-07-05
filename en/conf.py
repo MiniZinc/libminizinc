0c66e58b968ea9c580c274a7f6f924000affa244
Guido Tack <guido.tack@monash.edu>
2018-07-05 11:32:45 +1000

    Change to "read the docs" theme

--- en/conf.py
+++ en/conf.py
@@ -130,25 +130,23 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-import sphinx_bootstrap_theme
+import sphinx_rtd_theme
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
 
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    
-    'source_link_position': "",
-    
-    'globaltoc_depth': 3,
+  'logo_only' : False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
