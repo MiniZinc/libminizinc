8bf922dd968ccb7b39c0aae054ac0b07307b8ab8
Guido Tack <guido.tack@monash.edu>
2018-07-28 11:27:30 +1000

    Fix setup (did not load lexer any more)

--- en/conf.py
+++ en/conf.py
@@ -468,11 +468,9 @@ sphinx_to_github_verbose = True
 sphinx_to_github_encoding = "utf-8"
 
 def setup(sphinx):
+    import six
+    print('Six version: %s' % six.__version__)
     from minizinc_lexer import MznLexer, MznDefLexer
     sphinx.add_lexer("minizinc", MznLexer())
     sphinx.add_lexer("minizincdef", MznDefLexer())
     sphinx.add_stylesheet("style.css")
-
-def setup(app):
-    import six
-    print('Six version: %s' % six.__version__)
