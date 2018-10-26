1754cfd4306f509cf1c7710db99374d8ad2c1f08
Guido Tack <guido.tack@monash.edu>
2018-10-26 11:58:20 +1100

    Cannot use split index for Chinese version (triggers unicode bug in sphinx)

--- chi/conf.py
+++ chi/conf.py
@@ -214,7 +214,7 @@ html_static_path = ['_static']
 
 # If true, the index is split into individual pages for each letter.
 #
-html_split_index = True
+html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
 #
