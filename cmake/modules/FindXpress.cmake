165aedfe81be02f9bd902f5068b1fc4185cdfbbd
Jip J. Dekker <jip@dekker.one>
2018-11-29 14:33:33 +1100

    Add find module for FICO Xpress

---
+++ cmake/modules/FindXpress.cmake
@@ -0,0 +1,37 @@
+# - Try to find FICO Xpress
+# Once done this will define
+#  XPRESS_FOUND        - System has FICO Xpress
+#  XPRESS_INCLUDE_DIRS - The FICO Xpress include directories
+#  XPRESS_LIBRARIES    - The libraries needed to use FICO Xpress
+# User can set XPRESS_ROOT to the preferred installation prefix
+
+find_path(XPRESS_INCLUDE xprs.h
+          HINTS ${XPRESS_ROOT} $ENV{XPRESS_ROOT}
+          PATH $ENV{XPRESS_DIR} /opt/xpressmp
+          PATH_SUFFIXES include)
+
+
+foreach(XPRESS_LIB xprb xprs)
+  set(XPRESS_LIB_LOC "XPRESS_LIB_LOC-NOTFOUND")
+  find_library(XPRESS_LIB_LOC NAMES ${XPRESS_LIB} lib${XPRESS_LIB}
+               HINTS ${XPRESS_ROOT} $ENV{XPRESS_ROOT}
+               PATH $ENV{XPRESS_DIR} /opt/xpressmp
+               PATH_SUFFIXES lib)
+  if("${XPRESS_LIB_LOC}" STREQUAL "XPRESS_LIB_LOC-NOTFOUND")
+    message(STATUS "FICO Xpres: Could not find library `${XPRESS_LIB}`")
+    set(XPRESS_LIBRARY "")
+    break()
+  endif()
+  list(APPEND XPRESS_LIBRARY ${GECODE_LIB_LOC})
+endforeach(XPRESS_LIB)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set XPRESS_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(Xpres DEFAULT_MSG
+                                  XPRESS_INCLUDE XPRESS_LIBRARY)
+
+mark_as_advanced(XPRESS_INCLUDE XPRESS_LIBRARY)
+
+set(XPRESS_LIBRARIES ${XPRESS_LIBRARY})
+set(XPRESS_INCLUDE_DIRS ${XPRESS_INCLUDE})
---
+++ cmake/targets/libminizinc_xpress.cmake
@@ -0,0 +1,21 @@
+### MiniZinc FICO Xpress Solver Target
+
+find_package(Xpress)
+
+if(XPRESS_FOUND)
+
+  add_library(minizinc_xpress
+              solvers/MIP/MIP_solverinstance.cpp solvers/MIP/MIP_xpress_wrap.cpp solvers/MIP/MIP_xpress_solverfactory.cpp
+              include/minizinc/solvers/MIP/MIP_xpress_wrap.hh include/minizinc/solvers/MIP/MIP_xpress_solverfactory.hh
+              include/minizinc/solvers/MIP/MIP_solverinstance.hh include/minizinc/solvers/MIP/MIP_solverinstance.hpp)
+
+  target_include_directories(minizinc_xpress PRIVATE ${XPRESS_INCLUDE_DIRS})
+  target_link_libraries(minizinc_xpress minizinc xprb xprs ${CMAKE_THREAD_LIBS_INIT})
+
+  set(EXTRA_TARGETS ${EXTRA_TARGETS} minizinc_xpress)
+  INSTALL(TARGETS minizinc_xpress
+          EXPORT libminizincTargets
+          RUNTIME DESTINATION bin
+          LIBRARY DESTINATION lib
+          ARCHIVE DESTINATION lib)
+endif()
