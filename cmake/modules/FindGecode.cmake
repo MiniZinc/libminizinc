542ce7407f148fb5fdc4f471b14b0c4984547d32
Jip J. Dekker <jip@dekker.one>
2018-12-10 19:03:03 +1100

    Allow for the absence of the Gecode Gist library

--- cmake/modules/FindGecode.cmake
+++ cmake/modules/FindGecode.cmake
@@ -23,12 +23,14 @@ foreach(GECODE_LIB ${GECODE_REQ_LIBS})
   find_library(GECODE_LIB_LOC NAMES ${GECODE_LIB} ${GECODE_LIB_WIN}
                HINTS ${GECODE_ROOT} $ENV{GECODE_ROOT}
                PATH_SUFFIXES lib)
-  if("${GECODE_LIB_LOC}" STREQUAL "GECODE_LIB_LOC-NOTFOUND")
+  if("${GECODE_LIB_LOC}" STREQUAL "GECODE_LIB_LOC-NOTFOUND" AND NOT "${GECODE_LIB}" STREQUAL "gecodegist")
 #    message(STATUS "Gecode: Could not find library `${GECODE_LIB}`")
     set(GECODE_LIBRARY "")
     break()
   endif()
-  list(APPEND GECODE_LIBRARY ${GECODE_LIB_LOC})
+  if(NOT "${GECODE_LIB_LOC}" STREQUAL "GECODE_LIB_LOC-NOTFOUND")
+    list(APPEND GECODE_LIBRARY ${GECODE_LIB_LOC})
+  endif()
 endforeach(GECODE_LIB)
 
 unset(GECODE_REQ_LIBS)
