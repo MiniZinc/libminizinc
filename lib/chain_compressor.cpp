82a0f69477c9b05f58390166b3be8ba2c4edfd36
Guido Tack <guido.tack@monash.edu>
2019-02-13 08:43:16 +1100

    Handle variables with unbounded domains in chain compression

--- lib/chain_compressor.cpp
+++ lib/chain_compressor.cpp
@@ -499,7 +499,9 @@ namespace MiniZinc {
     IntSetVal* dom_b;
 
     if(auto a_decl = follow_id_to_decl(a)->dyn_cast<VarDecl>()) {
-      dom_a = eval_intset(env, a_decl->ti()->domain());
+      if (a_decl->ti()->domain()) {
+        dom_a = eval_intset(env, a_decl->ti()->domain());
+      }
     } else {
       assert(a->dyn_cast<IntLit>());
       auto a_val = a->cast<IntLit>();
@@ -507,7 +509,9 @@ namespace MiniZinc {
     }
 
     if(auto b_decl = follow_id_to_decl(b)->dyn_cast<VarDecl>()) {
-      dom_b = eval_intset(env, b_decl->ti()->domain());
+      if (b_decl->ti()->domain()) {
+        dom_b = eval_intset(env, b_decl->ti()->domain());
+      }
     } else {
       assert(b->dyn_cast<IntLit>());
       auto b_val = b->cast<IntLit>();
