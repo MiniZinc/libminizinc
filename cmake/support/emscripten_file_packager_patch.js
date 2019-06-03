85ff4a64254be211829fa1053ddcb5c128cc2343
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-06-03 13:09:10 +1000

    better patch to fix relative loading in browser

--- cmake/support/emscripten_file_packager_patch.js
+++ cmake/support/emscripten_file_packager_patch.js
@@ -22,4 +22,20 @@ if (filePackagerPatch_isNodeOrShell()) {
         pathname: '/'
     };
     Module.getPreloadedPackage = Module.getPreloadedPackage || filePackagerPatch_getPreloadedPackageNode;
+} else {
+    // need a hack to locate relative file in browser settings for the file packager
+    var wrappee = Module.locateFile || function(path, prefix) { return prefix + path };
+    Module.locateFile = function (path, prefix) {
+        if (prefix || !path.endsWith('.data')) {
+        return wrappee(path, prefix);
+        }
+        // file packager is called before a proper script location decection
+        var base = _scriptDir ? _scriptDir : (typeof importScripts === 'function' ? self.location.href : document.currentScript.src);
+        if (base.indexOf('blob:') !== 0) {
+        base = base.substr(0, base.lastIndexOf('/') + 1);
+        } else {
+        base = '';
+        }
+        return wrappee(path, base);
+    };
 }
