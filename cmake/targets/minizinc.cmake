2aa84d73f25ea07eb494513bde48a2a652017345
Jip J. Dekker <jip@dekker.one>
2019-06-06 11:45:24 +1000

    Fix MVSC linking error because of duplicate lib/exe output name

--- cmake/targets/minizinc.cmake
+++ cmake/targets/minizinc.cmake
@@ -2,7 +2,10 @@
 
 add_executable(minizinc_exe minizinc.cpp)
 target_link_libraries(minizinc_exe minizinc)
-set_target_properties(minizinc_exe PROPERTIES OUTPUT_NAME "minizinc")
+
+if(NOT WIN32) # TODO: Output name being the same as a library name gives a linker error with MVSC
+  set_target_properties(minizinc_exe PROPERTIES OUTPUT_NAME "minizinc")
+endif()
 
 install(
   TARGETS minizinc_exe
