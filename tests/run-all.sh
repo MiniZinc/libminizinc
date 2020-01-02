e776a560ed70b975bac03d2938dc026dfeb06aff
Jason Nguyen <jason.nguyen@monash.edu>
2020-01-03 10:03:24 +1100

    Remove old test suite

--- tests/run-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# set -x
-export PATH="$(pwd)/..":"$(pwd)/scripts":"$PATH"
-export MZN_STDLIB_DIR="$(pwd)/../share/minizinc"
-#run-tests mzn20_fd .mzn unit examples
-run-tests mzn-fzn_fd .mzn unit examples
-#run-tests mzn20_fd_linear .mzn unit examples
-#exec run-tests mzn20_mip .mzn unit examples
