d271948dd12e1880c3fa9e4e1376c76a9cc5ad56
Jip J. Dekker <jip@dekker.one>
2020-02-21 14:22:14 +1100

    Fix segmentation fault in output_only typechecking
    Ensure value has a right hand side before checking its value

--- lib/typecheck.cpp
+++ lib/typecheck.cpp
@@ -2216,7 +2216,7 @@ namespace MiniZinc {
             _typeErrors.push_back(TypeError(env,vdi->loc(),
                                             "set element type for `"+vdi->id()->str().str()+"' is not finite"));
           }
-          if (i->e()->ann().contains(constants().ann.output_only) && vdi->e()->type().isvar()) {
+          if (i->e()->ann().contains(constants().ann.output_only) && vdi->e() && vdi->e()->type().isvar()) {
             _typeErrors.push_back(TypeError(env,vdi->loc(),"variables annotated with ::output_only must be par"));
           }
         }
