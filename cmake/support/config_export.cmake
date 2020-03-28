504a8519d84209aec96ff4ea30bd0a2ec1f5d1cf
Fabio Pesari <fpesari@tuxfamily.org>
2020-03-28 17:30:53 +0100

    CMake fix for setting the proper library installation directory

--- cmake/support/config_export.cmake
+++ cmake/support/config_export.cmake
@@ -4,7 +4,7 @@
 if(WIN32 AND NOT CYGWIN)
   set(DEF_INSTALL_CMAKE_DIR CMake)
 else()
-  set(DEF_INSTALL_CMAKE_DIR lib/cmake)
+  set(DEF_INSTALL_CMAKE_DIR ${CMAKE_INSTALL_LIBDIR}/cmake)
 endif()
 set(INSTALL_CMAKE_DIR ${DEF_INSTALL_CMAKE_DIR} CACHE PATH "Installation directory for CMake files")
 
