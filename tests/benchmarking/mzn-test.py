3f683c68c2de0b7449770ee0532ba0e28510d5e4
Gleb Belov <bg37@gmx.net>
2020-07-03 12:50:19 +1000

    mzn-test: 8 GB mem limit

--- tests/benchmarking/mzn-test.py
+++ tests/benchmarking/mzn-test.py
@@ -226,7 +226,7 @@ class MZT_Param:
                   " Then you can do shell tricks but Ctrl+C may not kill all subprocesses etc."],
                 "n_TimeoutRealHard": [150, "/// Real-time timeout per instance, seconds,"
                   " for all solution steps together. Use mzn/backend options for CPU time limit."],
-                "n_VMEMLIMIT_SoftHard": [4000000, 4000000, "/// 2 limits, soft/hard, in KB. Platform-dependent in Python 3.6. Default 8 GB"],
+                "n_VMEMLIMIT_SoftHard": [8000000, 8000000, "/// 2 limits, soft/hard, in KB. Platform-dependent in Python 3.6. Default 8 GB"],
               },
               "Stderr_Keylines": {
                 s_CommentKey: [ "A complete line in stderr will be interpreted accordingly.",
