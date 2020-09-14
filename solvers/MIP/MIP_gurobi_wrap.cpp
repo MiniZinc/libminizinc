7675576df68437122b4de502b497b8aa1a962124
Gleb Belov <gleb.belov@monash.edu>
2020-09-14 15:59:36 +1000

    Gurobi: don't create gurobi-mzn.log

--- solvers/MIP/MIP_gurobi_wrap.cpp
+++ solvers/MIP/MIP_gurobi_wrap.cpp
@@ -365,7 +365,7 @@ void MIP_gurobi_wrapper::openGUROBI() {
   {
     //   cout << "% " << flush;               // Gurobi 7.5.2 prints "Academic License..."
     MiniZinc::StreamRedir redirStdout(stdout, stderr);
-    error = dll_GRBloadenv(&env, "gurobi-mzn.log");
+    error = dll_GRBloadenv(&env, nullptr);
   }
   wrap_assert(!error, "Could not open GUROBI environment.");
   error = dll_GRBsetintparam(env, "OutputFlag", 0);  // Switch off output
