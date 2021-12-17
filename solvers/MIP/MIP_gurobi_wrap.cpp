85d378828656673cca71090a70aaddf545293e5d
Jason N <admin@cyderize.org>
2021-12-17 11:05:58 +1100

    Find Gurobi 9.5.0 and potential future versions

--- solvers/MIP/MIP_gurobi_wrap.cpp
+++ solvers/MIP/MIP_gurobi_wrap.cpp
@@ -91,9 +91,9 @@ vector<string> MIPGurobiWrapper::getStdFlags() { return {"-i", "-p", "-s", "-v"}
 
 vector<string> gurobi_dlls() {
   const vector<string> versions = {
-      "913", "912", "911", "910", "904",  // Potential future versions which should load correctly
-      "903", "902", "901", "900", "811", "810", "801", "800", "752",
-      "751", "750", "702", "701", "700", "652", "651", "650"};
+      "954", "953", "952", "951",  // Potential future versions which should load correctly
+      "950", "912", "911", "910", "903", "902", "901", "900", "811", "810", "801",
+      "800", "752", "751", "750", "702", "701", "700", "652", "651", "650"};
   vector<string> dlls;
   string lastMajorVersion;
   for (const auto& version : versions) {
