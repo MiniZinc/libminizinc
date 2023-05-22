6fa4f300fafc5acfeff7a80f14baf8867a6b29b6
Jason N <admin@cyderize.org>
2023-05-22 15:01:12 +1000

    Update emscripten CMake config
    Need to manually add dependency for file packager output because `em_link_pre_js` doesn't work with `custom_target` files anymore.

--- cmake/support/config_emscripten.cmake
+++ cmake/support/config_emscripten.cmake
@@ -8,6 +8,11 @@ if (DEFINED EMSCRIPTEN)
                      COMMAND python3 ${EMSCRIPTEN_ROOT_PATH}/tools/file_packager.py minizinc.data --lz4 --preload ${PROJECT_SOURCE_DIR}/share/minizinc@usr/share/minizinc ${CMAKE_BINARY_DIR}/share/minizinc@usr/share/minizinc --from-emcc --js-output=${PROJECT_BINARY_DIR}/CMakeFiles/file_packager.js
                      COMMENT "building data store minizinc.data")
   
+  add_custom_target(file_packager DEPENDS ${PROJECT_BINARY_DIR}/CMakeFiles/file_packager.js)
+
+  add_dependencies(minizinc file_packager)
+  add_dependencies(mzn2doc file_packager)
+
   set(EMSCRIPTEN_CXX_FLAGS "-fexceptions")
   set(EMSCRIPTEN_LINK_FLAGS "-s FORCE_FILESYSTEM=1 -s LZ4=1 -s MODULARIZE=1 -s EXPORTED_RUNTIME_METHODS=callMain,cwrap,FS,ENV -s ALLOW_MEMORY_GROWTH=1 -s INITIAL_MEMORY=33554432 -s STACK_SIZE=4194304 -fexceptions --no-heap-copy")
 
@@ -38,9 +43,9 @@ if (DEFINED EMSCRIPTEN)
 
     # MiniZinc main executable
     add_executable(minizinc_asm minizinc.cpp)
+    add_dependencies(minizinc_asm file_packager)
     target_link_libraries(minizinc_asm mzn)
 
-    em_link_pre_js(minizinc_asm ${PROJECT_SOURCE_DIR}/cmake/support/emscripten_file_packager_patch.js)
     em_link_pre_js(minizinc_asm ${PROJECT_BINARY_DIR}/CMakeFiles/file_packager.js)
 
     set_target_properties(minizinc_asm PROPERTIES LINK_FLAGS "-s WASM=0 -s EXPORT_NAME=\"'MINIZINC'\" ${EMSCRIPTEN_LINK_FLAGS}")
@@ -50,9 +55,9 @@ if (DEFINED EMSCRIPTEN)
 
     # mzn2doc executable
     add_executable(mzn2doc_asm mzn2doc.cpp)
+    add_dependencies(mzn2doc_asm file_packager)
     target_link_libraries(mzn2doc_asm mzn)
 
-    em_link_pre_js(mzn2doc_asm ${PROJECT_SOURCE_DIR}/cmake/support/emscripten_file_packager_patch.js)
     em_link_pre_js(mzn2doc_asm ${PROJECT_BINARY_DIR}/CMakeFiles/file_packager.js)
 
     set_target_properties(mzn2doc_asm PROPERTIES LINK_FLAGS "-s WASM=0 -s EXPORT_NAME=\"'MZN2DOC'\" ${EMSCRIPTEN_LINK_FLAGS}")
