d8e41d06e40ca513bbef5c2a7ae7351321c401ec
Guido Tack <guido.tack@monash.edu>
2023-08-10 09:37:44 +1000

    Fix license text ("not" had been replaced by "!", probably by a search-and-replace, in the past)

--- lib/flattener.cpp
+++ lib/flattener.cpp
@@ -6,7 +6,7 @@
  */
 
 /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was ! distributed with this
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /* A basic mzn2fzn wrapper, can be used as a plugin
