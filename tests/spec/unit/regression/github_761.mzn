MiniZinc/libminizinc

commit ca7c5621f844fefce463a38896b6f57f89a75a97

Fix type for non-array return with array TIID

lib/type_specialise.cpp
@@ -447,6 +447,9 @@ public:
       }
     } else if (TIId* tiid = Expression::dynamicCast<TIId>(ti->domain())) {
       Type ret_type = ti_map.find(tiid->v())->second;
+      if (ret_type.dim() != 0 && tiid->type().dim() == 0) {
+        ret_type = ret_type.elemType(env);
+      }
       if (ret_type.structBT()) {
         ti->setStructDomain(env, ret_type, false, false);
       } else {

tests/spec/unit/general/array_param_non_array_return.mzn
@@ -1,0 +1,11 @@
+/***
+!Test
+solvers: [gecode]
+expected: !Result
+  status: SATISFIED
+  solution: !Solution
+    x: [1]
+***/
+
+function any $T: foo(array [int] of any $T: x) = x[1];
+any: x :: output = foo([(1,)]);

commit 3a9bc6ded0f293f3365f5ac134f1556deb34c7a2

Fix check for non-array return version of array TIID parameter
Fixes bug introduced in ca7c5621f844fefce463a38896b6f57f89a75a97.

Fixes #761.

lib/type_specialise.cpp
@@ -447,7 +447,7 @@ public:
       }
     } else if (TIId* tiid = Expression::dynamicCast<TIId>(ti->domain())) {
       Type ret_type = ti_map.find(tiid->v())->second;
-      if (ret_type.dim() != 0 && tiid->type().dim() == 0) {
+      if (ret_type.dim() != 0 && ti->type().dim() == 0) {
         ret_type = ret_type.elemType(env);
       }
       if (ret_type.structBT()) {

tests/spec/unit/regression/github_761.mzn
@@ -1,0 +1,11 @@
+/***
+!Test
+solvers: [gecode]
+expected: !Result
+  solution: !Solution
+***/
+
+% Regression test for GitHub #761
+
+function array [int] of any $T: foo(array [int] of any $T: x) = x;
+any: x = foo([(a: 1)]);