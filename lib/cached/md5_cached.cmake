a36be67171335a80563a487ec01e303b4725f0bc
Guido Tack <guido.tack@monash.edu>
2026-05-19 09:17:21 +0100

    Fix parsing of index-dependent array decls to exclude expressions like `c in int`

--- lib/cached/md5_cached.cmake
+++ lib/cached/md5_cached.cmake
@@ -1,4 +1,4 @@
 set(lexer_lxx_md5_cached "29c69cfc368018c0bf8fe3d63f00c677")
-set(parser_yxx_md5_cached "dea770003b555904ec408e964ec9497a")
+set(parser_yxx_md5_cached "21afb91ce0accadac4825bd063fc3e29")
 set(regex_lexer_lxx_md5_cached "d4bdc40f824511fb088b32f57405136c")
 set(regex_parser_yxx_md5_cached "e44695248dcfad64a56982f126f8e4f5")
